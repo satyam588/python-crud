fa1ca064777027a1bbd9c46bfa5520154ed0e5bb
Satyam Sundaram <sundaramsatyam12@gmail.com>
2022-06-12 19:25:34 +0530

    Commit

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-vxp%k21c3a(p&3$hozs4$5--t%)nrat&(u52chj(uhe(b92#b(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','crud-demo-python.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
