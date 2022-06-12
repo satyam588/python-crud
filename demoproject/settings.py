7b9137d652aeaef95267c2d4b26df244b1efe5fd
Satyam Sundaram <sundaramsatyam12@gmail.com>
2022-06-12 18:52:28 +0530

    Updated

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-vxp%k21c3a(p&3$hozs4$5--t%)nrat&(u52chj(uhe(b92#b("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','https://crud-demo-python.herokuapp.com/']
 
 
 # Application definition
